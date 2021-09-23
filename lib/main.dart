e981b9c938460350e103a915b600afd0f526c693
khush <khushp.itpath@gmail.com>
2021-09-23 10:13:44 +0530

    no sound null check main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
+  //--no-sound-null-safety
   @override
   Widget build(BuildContext context) {
     // Declare Variables here...
